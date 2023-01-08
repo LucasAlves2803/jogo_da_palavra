4b344844c1f3d603968a1d7a97c5cec555350a17
LucasAlves2803 <lucascosta2822@gmail.com>
2023-01-07 21:56:16 -0300

    jogo_da_palavra_beta

---
+++ jogo_da_palavra.py
@@ -0,0 +1,45 @@
+# jogo da palavra
+
+
+
+
+
+def eh_uma_substring(sub,palavra):
+    return sub in palavra
+def percorre_string(palavra2,palavra): #percorre uma substring
+    ind1 = 0
+    ind2 = ind1+3
+    while (len(palavra2)- ind1  > 2):
+        if (eh_uma_substring(palavra2[ind1:ind2],palavra)):
+            print(palavra2[ind1:ind2], "é uma substring de ", palavra)
+            return True
+        ind1+=1
+        ind2+=1
+    return False
+
+def main():
+    if (percorre_string(palavra_dois,palavra)):
+        print("Continua...")
+        return True
+    else:
+        print("Game Over")
+        return False
+
+
+#Main
+print("Digite uma palavra: ")
+global palavra
+palavra = str(input())
+print("Digite outra palavra: ")
+global palavra_dois
+palavra_dois = str(input())
+
+while(True):
+    if (main()):
+        palavra = palavra_dois
+        print("A nova palavra é ", palavra)
+        print("Digite outra palavra: ")
+        palavra_dois = str(input())
+    else:
+        break
+    
